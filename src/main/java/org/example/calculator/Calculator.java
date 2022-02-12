b0e7a1a061cdf05d6f56ddb1e319ead7b65284e2
PetterBT <petter.bergstrom@iths.se>
2022-02-13 00:27:51 +0100

    Make method return zero and add passing test

--- src/main/java/org/example/calculator/Calculator.java
+++ src/main/java/org/example/calculator/Calculator.java
@@ -1,7 +1,7 @@
 package org.example.calculator;
 
 public class Calculator {
-    public static void add() {
-
+    public int add(String input) {
+        return 0;
     }
 }
--- src/test/java/org/example/calculator/CalculatorTest.java
+++ src/test/java/org/example/calculator/CalculatorTest.java
@@ -1,8 +1,15 @@
 package org.example.calculator;
 
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CalculatorTest {
 
-    void shouldReturnInput() {
-        Calculator.add();
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void shouldReturnInput() {
+        assertThat(calculator.add("69")).isEqualTo(0);
     }
 }
